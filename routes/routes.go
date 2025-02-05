9b211a016cd6866a8227a78f2858a55b8c9be770
beopiyo <benardopiyo13@gmail.com>
2025-02-05 08:05:44 +0300

    feat: add more handlers and serve the static files

--- routes/routes.go
+++ routes/routes.go
@@ -8,7 +8,14 @@ import (
 func Router() *http.ServeMux {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", handlers.Home)
-	mux.HandleFunc("/login", handlers.Login)
 	mux.HandleFunc("/register", handlers.Register)
+	mux.HandleFunc("/login", handlers.Login)
+	mux.HandleFunc("/dashboard", handlers.Dashboard)
+	mux.HandleFunc("/logout", handlers.Logout)
+	mux.HandleFunc("/post", handlers.Post)
+	mux.HandleFunc("/comment", handlers.Comment)
+
+	fileserver := http.FileServer(http.Dir("../static"))
+	mux.Handle("/static/", http.StripPrefix("/static/", fileserver))
 	return mux
 }
